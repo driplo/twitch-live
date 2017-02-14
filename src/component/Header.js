572601945789435a55b719529b787de55746b086
AdrienAuer <aauer@silamir.com>
2017-02-14 16:09:17 +0100

    Style: player

--- src/component/Header.js
+++ src/component/Header.js
@@ -8,7 +8,7 @@ class Header extends Component {
     return(
       <div className="App-header">
         {/* <img src={logo_twitch} className="App-logo" alt="logo" /> */}
-        <h1>Twitch Storm</h1>
+        <h1>Twitch Switch</h1>
         <h3>Watch your favorite twitch streamers</h3>
       </div>
     )
--- src/component/Player.js
+++ src/component/Player.js
@@ -6,21 +6,59 @@ class Player extends Component {
 
     return(
       <div className="Player">
-        <div className="StreamList-container">
-          <div className="StreamList-header">
-            Following Channels
-          </div>
-          <div className="StreamList">
-            <div className="stream-entry">
-              
+        <div className="player-shadow"></div>
+        <div className="player-background"></div>
+        <div className="player-content">
+          <div className="StreamList-container SidePlayer">
+            <div className="StreamList-header">
+              Following Channels
+            </div>
+            <div className="StreamList">
+              <div className="stream-entry stream-entry--active">
+                <div className="stream-img">
+                  <img src="http://i.imgur.com/sP7hN84.png"  alt="stream-game" />
+                </div>
+                <div className="stream-column">
+                  <div className="streamer-name">Eclypsiatvlol</div>
+                  <div className="streamer-title">HIHI JBZZ L'OTP ZIGGS EN...</div>
+                </div>
+              </div>
+              <div className="stream-entry">
+                <div className="stream-img">
+                  <img src="http://i.imgur.com/sP7hN84.png"  alt="stream-game" />
+                </div>
+                <div className="stream-column">
+                  <div className="streamer-name">Eclypsiatvlol</div>
+                  <div className="streamer-title">Test de longueur de description du stream</div>
+                </div>
+              </div>
+              <div className="stream-entry">
+                <div className="stream-img">
+                  <img src="http://i.imgur.com/sP7hN84.png"  alt="stream-game" />
+                </div>
+                <div className="stream-column">
+                  <div className="streamer-name">Eclypsiatvlol</div>
+                  <div className="streamer-title">Lorem ipsum</div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="EmbedStream">
-          The stream
-        </div>
-        <div className="TwitchChat">
-          The chat
+          <div className="EmbedStream">
+            <iframe
+                src="http://player.twitch.tv/?channel=zeaeazeclypsiatvlol"
+                frameborder="0"
+                scrolling="no"
+                allowFullscreen="true">
+            </iframe>
+          </div>
+          <div className="TwitchChat SidePlayer">
+            <iframe 
+              frameborder="0" 
+              scrolling="no" 
+              id="chat_embed" 
+              src="http://www.twitch.tv/eclypsiatvlol/chat">
+            </iframe>
+          </div>
         </div>
       </div>
     )
