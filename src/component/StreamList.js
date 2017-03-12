c3ec6d7781727ffae143db2c8c8b30589636530b
Adrien AUER <draydrayz@gmail.com>
2017-03-12 22:38:50 +0100

    Refact: stream list

--- src/component/StreamList.js
+++ src/component/StreamList.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
-import StreamEntry from './StreamEntry';
 
 class StreamList extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {activeIndex: 0};
+  }
+  
+  handleItemClick(index) {
+    this.setState({activeIndex: index});
+  }
   
   render() {
 
     if (this.props.streams){
       return(
         <div className="StreamList">
-          {this.props.streams.map(stream =>
-            <StreamEntry stream={stream} key={stream.channel._id} />
+          {this.props.streams.map((stream, i) =>
+            <StreamEntry 
+              stream={stream} 
+              active={this.state.activeIndex === i}
+              onClick={this.handleItemClick.bind(this, i)}
+              key={stream.channel._id} />
            )}
         </div>
       )
@@ -26,5 +38,17 @@ class StreamList extends Component {
   }
     
 }
+
+const StreamEntry = ({onClick, stream, active}) => (
+  <div onClick={onClick} key={stream.channel._id} data-name={name} className={active? 'stream-entry stream-entry--active' : 'stream-entry'} title={stream.channel.status}>
+    <div className="stream-img">
+      <img src={stream.channel.logo} width="30" alt="stream-game" />
+    </div>
+    <div className="stream-column">
+      <div className="streamer-name">{stream.channel.display_name}<span className="streamer-game">{stream.channel.game}</span></div>
+      <div className="streamer-title">{stream.channel.status}</div>
+    </div>
+  </div>
+)
 
 export default StreamList;
